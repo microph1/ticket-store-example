7a5dc69d70d318144c4a46e886cf7ed1352eb7f0
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-10-11 10:58:09 +0200

    adding loading state for tickets

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketActions, TicketStore } from '../../services/tickets/ticket.store';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tickets',
@@ -8,8 +9,21 @@ import { TicketActions, TicketStore } from '../../services/tickets/ticket.store'
 export class TicketsComponent implements OnInit {
 
   public tickets$ = this.store.tickets$;
+  public loadingTickets: boolean;
 
-  constructor(private store: TicketStore) { }
+  constructor(private store: TicketStore) {
+    this.store.loading$.pipe(
+      filter((event) => {
+        return event.type === this.store.getRequestFromAction(TicketActions.FIND_ALL);
+      })
+    ).subscribe((status) => {
+
+      // we can't use an async pipe for this as it would subscribe to the observable after ngOnInit hence we miss
+      // the loading start event;
+      this.loadingTickets = status.status;
+
+    });
+  }
 
 
   public ngOnInit(): void {
